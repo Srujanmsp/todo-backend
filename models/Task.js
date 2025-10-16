fd4ea2c857dbdc229599d13c2620f9ac4f62b1e3
Srujan M S <srujanmsp@gmail.com>
2025-10-16 14:53:17 +0530

    category section added with enum and index

--- models/Task.js
+++ models/Task.js
@@ -4,6 +4,7 @@ const taskSchema = new mongoose.Schema({
 user: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true, index: true },
 title: { type: String, required: true },
 description: { type: String },
+category: { type: String, enum: ['Work','Personal','Shopping'], default: 'Personal', index: true },
 status: { type: String, enum: ['pending', 'completed'], default: 'pending', index: true },
 dueDate: { type: Date, index: true }
 }, { timestamps: true });
